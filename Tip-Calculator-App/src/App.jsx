13ad91c17a7076d93609885300a561bc8ad43c54
Viper <lmi25@txstate.edu>
2026-04-08 14:49:58 -0500

    Added starter files.

---
+++ Tip-Calculator-App/src/App.jsx
@@ -0,0 +1,30 @@
+export default function TipCalculator() {
+  return (
+    <div>
+      Bill
+
+      Select Tip %
+      5%
+      10%
+      15%
+      25%
+      50%
+      Custom
+
+      Number of People
+
+      Tip Amount
+      / person
+
+      Total
+      / person
+
+      Reset
+      
+      <footer class="attribution">
+        Challenge by <a href="https://www.frontendmentor.io?ref=challenge">Frontend Mentor</a>. 
+        Coded by <a href="#">Your Name Here</a>.
+      </footer>
+    </div>
+  )
+}
