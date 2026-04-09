0ea7a0ca46588a218e7359d0b5c214667626bdac
Viper <lmi25@txstate.edu>
2026-04-08 20:43:37 -0500

    Adjusted the layout spacing of the section container and the positioning of the last container so that it is towards the bottom. Furthermore, adjusted the color scheme, font-weights, and positioning of the footer.

--- Tip-Calculator-App/src/App.jsx
+++ Tip-Calculator-App/src/App.jsx
@@ -1,33 +1,12 @@
 export default function TipCalculator() {
-  /*
-  Bill
-
-      Select Tip %
-      5%
-      10%
-      15%
-      25%
-      50%
-      Custom
-
-      Number of People
-
-      Tip Amount
-      / person
-
-      Total
-      / person
-
-      Reset
-      */
 
   return (
-    <div className="parent-container grid grid-rows-[150px_1fr] grid-cols-1 gap-2 min-h-screen min-w-screen bg-teal-100 box-border">
+    <div className="parent-container grid grid-rows-[150px_1fr] grid-cols-1 gap-5 min-h-screen min-w-screen bg-teal-100 box-border">
       <header className="text-3xl flex items-center justify-center text-slate-500 uppercase font-medium tracking-[.5rem] p-[20px]">
         splitter
       </header>
 
-      <main className="bg-white grid grid-rows-2 grid-cols-1 items-start rounded-t-4xl w-full p-[40px]">
+      <main className="bg-white grid grid-rows-2 grid-cols-1 gap-5 items-start rounded-t-4xl w-full p-[40px]">
         <form action="" className="grid grid-rows-3 grid-cols-1 gap-10">
           <fieldset>
             <label htmlFor="bill-amount" className="font-bold tracking-widest text-md text-slate-500">Bill</label>
@@ -45,14 +24,14 @@ export default function TipCalculator() {
           </fieldset>
         </form>
 
-        <section className="bg-teal-800 rounded-xl p-[20px]">
+        <section className="bg-teal-800 rounded-xl p-[20px] place-self-end w-full flex flex-col gap-5">
           <div className="flex justify-between items-center">
             <div>
               <p className="text-white font-medium">Tip Amount</p>
               <p className="text-slate-400">/ person</p>
             </div>
             <div>
-              <p className="text-2xl text-teal-500 font-bold">$4.27</p>
+              <p className="text-3xl text-teal-500 font-bold">$4.27</p>
             </div>
           </div>
 
@@ -62,12 +41,14 @@ export default function TipCalculator() {
               <p className="text-slate-400">/ person</p>
             </div>
             <div>
-              <p className="text-2xl text-teal-500 font-bold">$32.79</p>
+              <p className="text-3xl text-teal-500 font-bold">$32.79</p>
             </div>
           </div>
+
+          <button className="uppercase bg-teal-500 rounded-sm w-full p-[10px] font-bold text-xl text-teal-900">reset</button>
         </section>
 
-        <footer className="attribution fixed bottom-[40px] text-center w-[250px]">
+        <footer className="attribution relative bottom-0 text-center w-[250px] place-self-center font-medium text-teal-900">
           Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank"><span className="font-medium text-blue-600">Frontend Mentor</span></a>. 
           Coded by <a href="#"><span className="font-medium text-blue-600" target="_blank">Joshua Martinez</span></a>.
         </footer>
